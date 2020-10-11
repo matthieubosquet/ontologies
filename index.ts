0103ff00bd3b5120dac1f0cd9021566f244fc6ff
Matthieu Bosquet <matthieubosquet@gmail.com>
2020-10-11 12:09:26 +0100

    npm

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { createReadStream } from 'fs';
+import { join as joinPath } from 'path';
+
+// Core W3C ontologies (OWL 2 reserved vocabulary)
+export const RDF: NodeJS.ReadableStream = createReadStream(joinPath(__dirname, `rdf.ttl`));
+export const RDFS: NodeJS.ReadableStream = createReadStream(joinPath(__dirname, `rdfs.ttl`));
+export const OWL: NodeJS.ReadableStream = createReadStream(joinPath(__dirname, `owl.ttl`));
+export const XSD: NodeJS.ReadableStream = createReadStream(joinPath(__dirname, `xsd.ttl`));
+
+// Other ontologies
+export const DC: NodeJS.ReadableStream = createReadStream(joinPath(__dirname, `dc.ttl`));
+
